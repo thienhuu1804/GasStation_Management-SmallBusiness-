a20a18d344af231165c535dd2bc84a524709ed9d
Nguyễn Thiên Hữu <nguyenthienhuu18041999@gmail.com>
2022-04-13 16:47:46 +0700

    Auto stash before checking out "origin/master"

--- src/gasstation_management/Main.java
+++ src/gasstation_management/Main.java
@@ -5,10 +5,13 @@
  */
 package gasstation_management;
 
+import gasstation_management.BUS.QuanLyTaiKhoan_BUS;
 import gasstation_management.DAO.QuanLyTaiKhoan_DAO;
 import gasstation_management.DTO.TaiKhoan;
 import gasstation_management.UI.MainPanel;
 import java.awt.BorderLayout;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
@@ -21,21 +24,25 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     public static void main(String[] args) {
+
         // Create mainframe
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        frame.setLayout(new BorderLayout());
+//
+//        MainPanel pnlMain = new MainPanel();
+//        //Làm gì thì add giao diện đó vào pnlMain
+//
+//        frame.setSize(pnlMain.getPreferredSize());
+//        frame.setLocationRelativeTo(null);
+//        frame.add(pnlMain);
+//        frame.setVisible(true);
+        QuanLyTaiKhoan_BUS qltk = new QuanLyTaiKhoan_BUS();
+        System.out.println(qltk.search("none").get(0).getMaNV());
 
-        MainPanel pnlMain = new MainPanel();
-        //Làm gì thì add giao diện đó vào pnlMain
-
-        frame.setSize(pnlMain.getPreferredSize());
-        frame.setLocationRelativeTo(null);
-        frame.add(pnlMain);
-        frame.setVisible(true);
-//        QuanLyTaiKhoan_DAO qltk = new QuanLyTaiKhoan_DAO();
-//        System.out.println(qltk.verifyPwd("abc", qltk.getHashedPwd("abc")));
     }
 
 }
