dada855d3f281c35c9a967525a254b4aaa41e9cf
Nguyễn Thiên Hữu <nguyenthienhuu18041999@gmail.com>
2022-04-13 14:46:08 +0700

    fix: some function from DAO to BUS

--- src/gasstation_management/DAO/QuanLyTaiKhoan_DAO.java
+++ src/gasstation_management/DAO/QuanLyTaiKhoan_DAO.java
@@ -23,32 +23,4 @@ public class QuanLyTaiKhoan_DAO {
     ArrayList<TaiKhoan> list_Account = new ArrayList<>();
 
     // mã hoá chuỗi pwd
-    public String getHashedPwd(String inputPwd) {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        return getHashedPwd(inputPwd, salt);
-    }
-
-    // Mã hoá chuỗi pwd khi có salt (Dùng khi muốn verify)
-    public String getHashedPwd(String inputPwd, byte[] salt) {
-        byte[] hashedPassword = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            md.update(salt);
-            hashedPassword = md.digest(inputPwd.getBytes(StandardCharsets.UTF_8));
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(TaiKhoan.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        // Pwd lưu lại có gắn salt ở đầu để sau này lấy ra verify
-        return Base64.getEncoder().encodeToString(salt) + "$" + Base64.getEncoder().encodeToString(hashedPassword);
-    }
-
-    // Verify pwd. inputPwd: pwd nhận thẳng từ người dùng. hashedPwd: pwd lưu trong db
-    public boolean verifyPwd(String inputPwd, String hashedPwd) {
-        byte[] salt = Base64.getDecoder().decode(hashedPwd.split("\\$")[0].getBytes());
-        String hashedInput = getHashedPwd(inputPwd, salt);
-        System.out.println(hashedInput);
-        return hashedInput.equalsIgnoreCase(hashedPwd);
-    }
 }
