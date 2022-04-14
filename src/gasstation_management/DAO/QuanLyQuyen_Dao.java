ea9c5fb99103696e7ddb9081f15e0a8a714df908
Trần Quốc Vương <tranquocvuong0603@gmail.com>
2022-04-14 15:21:47 +0700

    Update QuanLyQuyen_Dao.java

--- src/gasstation_management/DAO/QuanLyQuyen_Dao.java
+++ src/gasstation_management/DAO/QuanLyQuyen_Dao.java
@@ -18,19 +18,20 @@ import java.sql.PreparedStatement;
  */
 public class QuanLyQuyen_Dao {
 
-    private String qry;
+    DBConnect conn = new DBConnect();
+    ResultSet rs;
     public QuanLyQuyen_Dao() {
         
         
     }
-    public String[][] LoadDataAccount() throws SQLException
+    public String[][] LoadDataAccount()
     {
         int count;
-        String qry = "select tendangnhap,quyen.motaquyen from taikhoan_quyen,quyen where taikhoan_quyen.maquyen=quyen.maquyen";
-       DBConnect db = new DBConnect();
-       ResultSet rs = db.getInforPowerOfAllAccount(qry);
        count =1;
        String[][] result = new String[0][3];
+        try {
+            PreparedStatement stm = conn.getConnection().prepareStatement("select tendangnhap,quyen.motaquyen from taikhoan_quyen,quyen where taikhoan_quyen.maquyen=quyen.maquyen");
+            rs = stm.executeQuery();
             while (rs.next()) { // will traverse through all rows
                        boolean flag = false;
                 ArrayList<String> list = new ArrayList<String>();
@@ -68,6 +69,8 @@ public class QuanLyQuyen_Dao {
                 }
             }
             return result;
-         
-}
+        } catch (Exception e) {
+            return null;
+        }
+        }
 }
