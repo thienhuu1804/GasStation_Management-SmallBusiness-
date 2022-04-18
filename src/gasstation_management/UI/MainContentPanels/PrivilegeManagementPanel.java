b95228328ec1985a2330c635b198b95ad1ea5b04
Trần Quốc Vương <tranquocvuong0603@gmail.com>
2022-04-18 07:59:18 +0700

    ..

--- src/gasstation_management/UI/MainContentPanels/PrivilegeManagementPanel.java
+++ src/gasstation_management/UI/MainContentPanels/PrivilegeManagementPanel.java
@@ -114,7 +114,7 @@ public class PrivilegeManagementPanel extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(37, 37, 37)
                         .addComponent(btnSuaQuyen, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
                 .addComponent(scrollTable, javax.swing.GroupLayout.PREFERRED_SIZE, 402, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
