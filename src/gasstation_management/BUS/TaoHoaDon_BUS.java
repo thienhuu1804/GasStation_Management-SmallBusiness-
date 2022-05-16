4fb490c178bd64306e1a5057efaaffc85116c52e
tfong83 <phongdicho83@gmail.com>
2022-05-16 23:34:03 +0700

    add TaoHoaDon, date HoaDon -> String

---
+++ src/gasstation_management/BUS/TaoHoaDon_BUS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gasstation_management.BUS;
+
+import gasstation_management.DAO.TaoHoaDon_DAO;
+import gasstation_management.DTO.HoaDon;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Utech
+ */
+public class TaoHoaDon_BUS {
+
+    TaoHoaDon_DAO taoHoaDon_DAO = new TaoHoaDon_DAO();
+
+    public ArrayList<HoaDon> getDanhSachHoaDon() {
+        return taoHoaDon_DAO.getDanhSachHoaDon();
+    }
+
+    public boolean addHoaDon(HoaDon hd) {
+        return taoHoaDon_DAO.addHoaDon(hd);
+    }
+}
